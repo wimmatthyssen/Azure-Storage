e26ab50af428d165d32ca6eaf407319ec0e64dff
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2023-04-18 12:50:00 +0200

    Update Create-Azure-Blob-Storage-SFTP-local-user-with-password-authentication.ps1

--- Create-Azure-Blob-Storage-SFTP-local-user-with-password-authentication.ps1
+++ Create-Azure-Blob-Storage-SFTP-local-user-with-password-authentication.ps1
@@ -37,7 +37,7 @@ Set-AzContext -tenantID "xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxx" (if not using the defa
 
 .LINK
 
-https://wmatthyssen.com/2023/04/04/create-an-sftp-enabled-azure-storage-account-within-a-specified-subscription-using-an-azure-powershell-script/
+https://wmatthyssen.com/2023/04/18/create-a-local-user-with-password-authentication-for-a-sftp-enabled-storage-account-using-an-azure-powershell-script/
 #>
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
